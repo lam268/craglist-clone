3c74a6ae50adca0992fb5dd2d5bbdefac6dd76a1
lam268 <lamlevu26@gmail.com>
2020-07-13 00:26:00 +0700

    init commit

---
+++ cragclone/craglist/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CraglistConfig(AppConfig):
+    name = 'craglist'
---
+++ cragclone/craglist/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'base.html')
